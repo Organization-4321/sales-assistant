972104266c41eff090ede5f4bac97b808a6edd81
xennvoid <thundvgod@gmail.com>
2024-09-06 20:00:25 +0300

    add AuthPage to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CssBaseline } from '@mui/material';
 import { ThemeContextProvider } from './theme';
+import Auth from './pages/Auth';
 
 function App() {
     return (
         <ThemeContextProvider>
             <CssBaseline />
+            <Auth />
         </ThemeContextProvider>
     );
 }
