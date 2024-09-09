ec11193e70de3bc13456dde46edd194996a83018
xennvoid <thundvgod@gmail.com>
2024-09-09 19:25:28 +0300

    chamge login builder mutation response type to generic

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -2,13 +2,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { AuthRoutes } from '../interfaces-submodule/enums/routes/auth-routes.enum';
 import { ILoginRequestDTO } from '../interfaces-submodule/interfaces/dto/auth/iadmin-login-request.interface';
 import { ILoginResponseDTO } from '../interfaces-submodule/interfaces/dto/auth/ilogin-response.interfaces';
+import { IApiResponseGenericDTO } from '../interfaces-submodule/interfaces/dto/common/iapi-response.interface';
 
 const loginApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: import.meta.env.VITE_BASE_API_URL + AuthRoutes.BasePrefix,
     }),
     endpoints: (builder) => ({
-        login: builder.mutation<ILoginResponseDTO, ILoginRequestDTO>({
+        login: builder.mutation<IApiResponseGenericDTO<ILoginResponseDTO>, ILoginRequestDTO>({
             query: ({ email, password }) => ({
                 url: AuthRoutes.Login,
                 method: 'POST',
