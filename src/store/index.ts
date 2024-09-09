dc62baef59775e777ce3068492514844910c2e66
xennvoid <thundvgod@gmail.com>
2024-09-09 16:19:00 +0300

    add redux RootState, AppDispatch types and typed hook useAppDispatch

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginApi from '../api/loginApi';
 import authReducer from './slices/authSlice';
+import { useDispatch } from 'react-redux';
 
 export const store = configureStore({
     reducer: {
@@ -9,3 +10,7 @@ export const store = configureStore({
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loginApi.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
