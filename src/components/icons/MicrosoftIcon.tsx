67f7fc57d8710db128e22eecf79da104045f53ff
xennvoid <thundvgod@gmail.com>
2024-09-16 12:44:12 +0300

    add microsoft icon component

---
+++ src/components/icons/MicrosoftIcon.tsx
@@ -0,0 +1,13 @@
+import { createSvgIcon } from '@mui/material';
+
+const MicrosoftIcon = createSvgIcon(
+    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11.9 0H0.5V11.4004H11.9V0Z" fill="#F25022" />
+        <path d="M24.4996 0H13.0996V11.4004H24.4996V0Z" fill="#7FBA00" />
+        <path d="M11.9 12.5996H0.5V24H11.9V12.5996Z" fill="#00A4EF" />
+        <path d="M24.4996 12.5996H13.0996V24H24.4996V12.5996Z" fill="#FFB900" />
+    </svg>,
+    'MicrosoftIcon',
+);
+
+export default MicrosoftIcon;
