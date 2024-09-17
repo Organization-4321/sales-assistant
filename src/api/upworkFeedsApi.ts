0e2736fb607df8648b895865fc4bd3eaee2c9bd7
xennvoid <thundvgod@gmail.com>
2024-09-17 16:18:36 +0300

    add getUpworkFeeds mutation to upworkFeedsApi

--- src/api/upworkFeedsApi.ts
+++ src/api/upworkFeedsApi.ts
@@ -1,11 +1,27 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { baseQueryWithReauth } from './queries';
 import { UpworkFeedsRoutesEnum } from '../interfaces-submodule/enums/routes/upwork-feeds-routes.enum';
+import { IApiResponseGenericDTO } from '../interfaces-submodule/interfaces/dto/common/iapi-response.interface';
+import { IUpworkResponseListFeedsDto } from '../interfaces-submodule/interfaces/dto/upwork-feed/iupwork-response-list-feeds.dto';
+import { IGetAllUpworkFeedRequest } from '../interfaces-submodule/interfaces/dto/upwork-feed/iget-all-upwork-feed-request.interface';
 
 const upworkFeedsApi = createApi({
     reducerPath: UpworkFeedsRoutesEnum.BasePrefix,
     baseQuery: baseQueryWithReauth(UpworkFeedsRoutesEnum.BasePrefix),
-    endpoints: (builder) => ({}),
+    endpoints: (builder) => ({
+        getUpworkFeeds: builder.mutation<
+            IApiResponseGenericDTO<IUpworkResponseListFeedsDto>,
+            IGetAllUpworkFeedRequest
+        >({
+            query: (requestBody) => ({
+                url: UpworkFeedsRoutesEnum.GetFeeds,
+                method: 'POST',
+                body: requestBody,
+            }),
+        }),
+    }),
 });
+
+export const { useGetUpworkFeedsMutation } = upworkFeedsApi;
 
 export default upworkFeedsApi;
