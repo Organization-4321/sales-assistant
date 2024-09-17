d4e6d6564cff94f83c4fbd2f94babb481583ad37
xennvoid <thundvgod@gmail.com>
2024-09-17 22:00:39 +0300

    add CustomTable component

---
+++ src/components/UI/CustomTable/index.tsx
@@ -0,0 +1,49 @@
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+
+interface ICustomTableProps<T extends object> {
+    data: T[];
+    columns: ColumnDef<T>[];
+}
+
+const CustomTable = <T extends object>({ data, columns }: ICustomTableProps<T>) => {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+    return (
+        <Table>
+            <TableHead>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                            <TableCell key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                          header.column.columnDef.header,
+                                          header.getContext(),
+                                      )}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableHead>
+            <TableBody>
+                {table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                            <TableCell key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
+};
+
+export default CustomTable;
