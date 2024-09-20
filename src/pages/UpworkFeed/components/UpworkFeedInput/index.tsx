ce2c1e94ef2a2b7ccef7a26035fd65c19afdc64b
xennvoid <thundvgod@gmail.com>
2024-09-20 14:31:35 +0300

    add UpworkFeedInput component

---
+++ src/pages/UpworkFeed/components/UpworkFeedInput/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { TextField, TextFieldProps } from '@mui/material';
+import ClearIcon from '../../../../components/icons/ClearIcon';
+
+type IUpworkFeedInputProps = TextFieldProps & {
+    onClearIconClick?: () => void;
+};
+
+const UpworkFeedInput: FC<IUpworkFeedInputProps> = ({ onClearIconClick, ...otherProps }) => {
+    return (
+        <TextField
+            fullWidth
+            InputProps={{
+                sx(theme) {
+                    return { height: theme.spacing(5.5) };
+                },
+                endAdornment: !!otherProps.value && (
+                    <ClearIcon
+                        sx={{ height: 18, width: 18, cursor: 'pointer' }}
+                        onClick={onClearIconClick}
+                    />
+                ),
+            }}
+            {...otherProps}
+        />
+    );
+};
+
+export default UpworkFeedInput;
