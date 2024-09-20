cfadbcbba0d5345fbfe603f2ccc5c3caa90c7007
xennvoid <thundvgod@gmail.com>
2024-09-20 17:58:19 +0300

    add ArrowIconLeft component

---
+++ src/components/icons/ArrowIconLeft.tsx
@@ -0,0 +1,15 @@
+import { createSvgIcon } from '@mui/material';
+
+const ArrowIconLeft = createSvgIcon(
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M6.50424 8L11.0042 12.5L9.50424 14L4 8.5V7.5L9.50424 2L11.0042 3.5L6.50424 8Z"
+            fill="#333333"
+        />
+    </svg>,
+    'ArrowIconLeft',
+);
+
+export default ArrowIconLeft;
