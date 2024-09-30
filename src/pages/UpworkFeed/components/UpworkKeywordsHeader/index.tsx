c56a132eb776b0cf85e651203851d1c0dad4bcc1
xennvoid <thundvgod@gmail.com>
2024-09-30 13:43:54 +0300

    change UpworkKeywordsHeader props types and replace select with CustomSelect

--- src/pages/UpworkFeed/components/UpworkKeywordsHeader/index.tsx
+++ src/pages/UpworkFeed/components/UpworkKeywordsHeader/index.tsx
@@ -1,20 +1,30 @@
 import { Box, Typography } from '@mui/material';
 import { FC } from 'react';
-import ReactSelect from 'react-select';
 import { IOptionInterface } from '../../../../interfaces-submodule/interfaces/dto/common/ioption.interface';
-
+import CustomSelect from '../../../../components/UI/CustomSelect';
 interface UpworkKeywordsHeaderProps {
     title: string;
     keywordsOptions: IOptionInterface[];
+    selectedOptions: IOptionInterface[];
+    setSelectedOptions: React.Dispatch<React.SetStateAction<IOptionInterface[]>>;
 }
 
-const UpworkKeywordsHeader: FC<UpworkKeywordsHeaderProps> = ({ title, keywordsOptions }) => {
+const UpworkKeywordsHeader: FC<UpworkKeywordsHeaderProps> = ({
+    title,
+    keywordsOptions,
+    selectedOptions,
+    setSelectedOptions,
+}) => {
     return (
         <Box display="flex" flexDirection="column" gap={4}>
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Typography variant="subtitle2">{title}</Typography>
             </Box>
-            <ReactSelect options={keywordsOptions} />
+            <CustomSelect
+                options={keywordsOptions}
+                selectedOptions={selectedOptions}
+                setSelectedOptions={setSelectedOptions}
+            />
         </Box>
     );
 };
