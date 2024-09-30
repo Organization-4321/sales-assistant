880a698412ef43ed57004131c65a7865490edf8f
xennvoid <thundvgod@gmail.com>
2024-09-30 13:45:04 +0300

    change UpworkScoreHeader props types and replace select with CustomSelect

--- src/pages/UpworkFeed/components/UpworkScoreHeader/index.tsx
+++ src/pages/UpworkFeed/components/UpworkScoreHeader/index.tsx
@@ -1,15 +1,22 @@
 import { FC } from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import SortDirectionIcon from '../../../../components/icons/SortDirectionIcon';
-import ReactSelect from 'react-select';
 import { IOptionInterface } from '../../../../interfaces-submodule/interfaces/dto/common/ioption.interface';
+import CustomSelect from '../../../../components/UI/CustomSelect';
 
 interface UpworkScoreHeaderProps {
     title: string;
     scoresOptions: IOptionInterface[];
+    selectedOptions: IOptionInterface[];
+    setSelectedOptions: React.Dispatch<React.SetStateAction<IOptionInterface[]>>;
 }
 
-const UpworkScoreHeader: FC<UpworkScoreHeaderProps> = ({ title, scoresOptions }) => {
+const UpworkScoreHeader: FC<UpworkScoreHeaderProps> = ({
+    title,
+    scoresOptions,
+    selectedOptions,
+    setSelectedOptions,
+}) => {
     return (
         <Grid container direction="column" gap={4}>
             <Grid item>
@@ -19,7 +26,11 @@ const UpworkScoreHeader: FC<UpworkScoreHeaderProps> = ({ title, scoresOptions })
                 </Box>
             </Grid>
             <Grid item>
-                <ReactSelect options={scoresOptions} />
+                <CustomSelect
+                    options={scoresOptions}
+                    selectedOptions={selectedOptions}
+                    setSelectedOptions={setSelectedOptions}
+                />
             </Grid>
         </Grid>
     );
