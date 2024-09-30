f1718dfc83f91b137b1450dbf679dc8b8d155409
xennvoid <thundvgod@gmail.com>
2024-09-30 13:49:16 +0300

    change UpworkPulishedHeader props types and provide necessary props to datepicker

--- src/pages/UpworkFeed/components/UpworkPublishedHeader/index.tsx
+++ src/pages/UpworkFeed/components/UpworkPublishedHeader/index.tsx
@@ -5,16 +5,18 @@ import CustomDatePicker from '../../../../components/UI/CustomDatePicker';
 
 interface UpworkPublishedHeaderProps {
     title: string;
+    date: Date | null;
+    setDate: React.Dispatch<React.SetStateAction<Date | null>>;
 }
 
-const UpworkPublishedHeader: FC<UpworkPublishedHeaderProps> = ({ title }) => {
+const UpworkPublishedHeader: FC<UpworkPublishedHeaderProps> = ({ title, date, setDate }) => {
     return (
         <Box display="flex" flexDirection="column" gap={4}>
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Typography variant="subtitle2">{title}</Typography>
                 <SortDirectionIcon />
             </Box>
-            <CustomDatePicker />
+            <CustomDatePicker selected={date} onChange={(date) => setDate(date)} />
         </Box>
     );
 };
