356cf6d9f1a79d07000f687baf8fb24cdcd4b20f
xennvoid <thundvgod@gmail.com>
2024-09-30 14:13:28 +0300

    change CustomTable props types and replace header table cells with Upwork Headers components

--- src/pages/UpworkFeed/components/CustomTable/index.tsx
+++ src/pages/UpworkFeed/components/CustomTable/index.tsx
@@ -1,12 +1,40 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { IOptionInterface } from '../../../../interfaces-submodule/interfaces/dto/common/ioption.interface';
+import UpworkTitleHeader from '../UpworkTitleHeader';
+import UpworkPublishedHeader from '../UpworkPublishedHeader';
+import UpworkKeywordsHeader from '../UpworkKeywordsHeader';
+import UpworkScoreHeader from '../UpworkScoreHeader';
 
 interface ICustomTableProps<T extends object> {
     data: T[];
     columns: ColumnDef<T>[];
+    title: string;
+    setTitle: React.Dispatch<React.SetStateAction<string>>;
+    keywordsOptions: IOptionInterface[];
+    scoresOptions: IOptionInterface[];
+    selectedKeywordsOptions: IOptionInterface[];
+    setSelectedKeywordsOptions: React.Dispatch<React.SetStateAction<IOptionInterface[]>>;
+    selectedScoresOptions: IOptionInterface[];
+    setSelectedScoresOptions: React.Dispatch<React.SetStateAction<IOptionInterface[]>>;
+    published: Date | null;
+    setPublished: React.Dispatch<React.SetStateAction<Date | null>>;
 }
 
-const CustomTable = <T extends object>({ data, columns }: ICustomTableProps<T>) => {
+const CustomTable = <T extends object>({
+    data,
+    columns,
+    title,
+    setTitle,
+    keywordsOptions,
+    scoresOptions,
+    selectedKeywordsOptions,
+    setSelectedKeywordsOptions,
+    selectedScoresOptions,
+    setSelectedScoresOptions,
+    published,
+    setPublished,
+}: ICustomTableProps<T>) => {
     const table = useReactTable({
         data,
         columns,
@@ -20,12 +48,54 @@ const CustomTable = <T extends object>({ data, columns }: ICustomTableProps<T>) 
                     <TableRow key={headerGroup.id}>
                         {headerGroup.headers.map((header) => (
                             <TableCell key={header.id}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                          header.column.columnDef.header,
-                                          header.getContext(),
-                                      )}
+                                {
+                                    {
+                                        title: (
+                                            <UpworkTitleHeader
+                                                title="Title"
+                                                inputValue={title}
+                                                setInputValue={setTitle}
+                                            />
+                                        ),
+                                        published: (
+                                            <UpworkPublishedHeader
+                                                title="Published"
+                                                date={published}
+                                                setDate={setPublished}
+                                            />
+                                        ),
+                                        keywords: (
+                                            <UpworkKeywordsHeader
+                                                title="Keywords"
+                                                keywordsOptions={keywordsOptions}
+                                                selectedOptions={selectedKeywordsOptions}
+                                                setSelectedOptions={setSelectedKeywordsOptions}
+                                            />
+                                        ),
+                                        score: (
+                                            <UpworkScoreHeader
+                                                title="Score"
+                                                scoresOptions={scoresOptions}
+                                                selectedOptions={selectedScoresOptions}
+                                                setSelectedOptions={setSelectedScoresOptions}
+                                            />
+                                        ),
+                                        matchedCases: (
+                                            <Typography
+                                                variant="subtitle2"
+                                                sx={{ textAlign: 'right' }}>
+                                                Matched cases
+                                            </Typography>
+                                        ),
+                                        matchedBlogs: (
+                                            <Typography
+                                                variant="subtitle2"
+                                                sx={{ textAlign: 'right' }}>
+                                                Matched blogs
+                                            </Typography>
+                                        ),
+                                    }[header.column.id]
+                                }
                             </TableCell>
                         ))}
                     </TableRow>
