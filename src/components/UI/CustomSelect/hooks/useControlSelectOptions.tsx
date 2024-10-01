754d4fe6b03086d072ad2f96ebd56e8367fbd407
xennvoid <thundvgod@gmail.com>
2024-10-01 11:35:31 +0300

    change handleChangeSelectedOptions function to fix lags

--- src/components/UI/CustomSelect/hooks/useControlSelectOptions.tsx
+++ src/components/UI/CustomSelect/hooks/useControlSelectOptions.tsx
@@ -1,20 +1,45 @@
 import { useState } from 'react';
 import { IOptionInterface } from '../../../../interfaces-submodule/interfaces/dto/common/ioption.interface';
+import { ActionMeta } from 'react-select';
 
 const useControlSelectOptions = (
     options: IOptionInterface[],
     selectedOptions: IOptionInterface[],
     setSelectedOptions: React.Dispatch<React.SetStateAction<IOptionInterface[]>>,
 ) => {
-    const [allOptionsSelected, setAllOptionsSelected] = useState(
-        selectedOptions.length === options.length,
-    );
+    const [allOptionsSelected, setAllOptionsSelected] = useState(false);
 
-    const handleChangeSelectedOptions = (newValue: IOptionInterface[]) => {
-        if (newValue.length === options.length) setAllOptionsSelected(true);
-        else setAllOptionsSelected(false);
+    const handleChangeSelectedOptions = (
+        newValue: IOptionInterface[],
+        actionMeta: ActionMeta<IOptionInterface>,
+    ) => {
+        if (!actionMeta || !actionMeta.option) return;
 
-        setSelectedOptions(newValue);
+        const action = actionMeta.action;
+
+        if (
+            action === 'select-option' &&
+            !selectedOptions.find(({ value }) => value === actionMeta.option?.value)
+        ) {
+            const newSelectedOptions = [...selectedOptions, actionMeta.option];
+            setSelectedOptions(newSelectedOptions);
+            if (options.length === newSelectedOptions.length) setAllOptionsSelected(true);
+            return;
+        } else if (
+            action === 'deselect-option' &&
+            !selectedOptions.find(({ value }) => value === actionMeta.option?.value)
+        ) {
+            const newSelectedOptions = [...selectedOptions, actionMeta.option];
+            setSelectedOptions(newSelectedOptions);
+            if (options.length === newSelectedOptions.length) setAllOptionsSelected(true);
+            return;
+        } else {
+            const newSelectedOptions = selectedOptions.filter(
+                ({ value }) => value !== actionMeta.option?.value,
+            );
+            setSelectedOptions(newSelectedOptions);
+            setAllOptionsSelected(false);
+        }
     };
 
     const handleToggleAllOptionsSelected = (
