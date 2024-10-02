211208a9890dd8258918600960e56979626dfacf
xennvoid <thundvgod@gmail.com>
2024-10-02 12:23:34 +0300

    change published state default value to null

--- src/pages/UpworkFeed/hooks/useUpworkFeedFilters.ts
+++ src/pages/UpworkFeed/hooks/useUpworkFeedFilters.ts
@@ -3,7 +3,7 @@ import { IOptionInterface } from '../../../interfaces-submodule/interfaces/dto/c
 
 const useUpworkFeedFilters = () => {
     const [title, setTitle] = useState('');
-    const [published, setPublished] = useState<Date | null>(new Date());
+    const [published, setPublished] = useState<Date | null>(null);
     const [selectedKeywordsOptions, setSelectedKeywordsOptions] = useState<IOptionInterface[]>([]);
     const [selectedScoresOptions, setSelectedScoresOptions] = useState<IOptionInterface[]>([]);
 
