ca1c2bef2e3040ae3d98838134207cf0ad5bfd88
xennvoid <thundvgod@gmail.com>
2024-10-03 17:40:48 +0300

    extract refetchUpworkFeeds params creation logic to utility function

--- src/pages/UpworkFeed/hooks/useUpworkFeedRequests.ts
+++ src/pages/UpworkFeed/hooks/useUpworkFeedRequests.ts
@@ -1,12 +1,11 @@
 import { useEffect } from 'react';
 import { useGetUpworkFeedsMutation } from '../../../api/upworkFeedsApi';
-import { UpworkFeedSearchBy } from '../../../interfaces-submodule/enums/upwork-feed/upwork-feed-search-by.enum';
-import { ISearchParameterDTO } from '../../../interfaces-submodule/interfaces/dto/common/isearch-parameter.interface';
 import { IOptionInterface } from '../../../interfaces-submodule/interfaces/dto/common/ioption.interface';
 import { UpworkFeedSortBy } from '../../../interfaces-submodule/enums/upwork-feed/upwork-feed-sort-by.enum';
 import { SortDirection } from '../../../interfaces-submodule/enums/common/sort-direction.enum';
+import createUpworkFeedsSearchParams from '../utils/createUpworkFeedsSearchParams';
 
-interface IUpworkFeedRefetchSearchParams {
+export interface IUpworkFeedRefetchSearchParams {
     title: string;
     published: Date | null;
     selectedKeywordsOptions: IOptionInterface[];
@@ -26,36 +25,7 @@ const useUpworkFeedRequests = (
     }, []);
 
     const refetchUpworkFeeds = () => {
-        const {
-            title,
-            published,
-            selectedKeywordsOptions,
-            selectedScoresOptions,
-            selectedReactionsOptions,
-        } = refetchSearchParams;
-
-        const searchParameters = [
-            title && { searchBy: UpworkFeedSearchBy.Title, searchQuery: title },
-            published && {
-                searchBy: UpworkFeedSearchBy.Published,
-                searchQuery: `${published.toISOString()} - ${new Date().toISOString()}`,
-            },
-            selectedKeywordsOptions.length > 0 && {
-                searchBy: UpworkFeedSearchBy.Keywords,
-                searchQuery: selectedKeywordsOptions.map((option) => option.value),
-            },
-            selectedScoresOptions.length > 0 && {
-                searchBy: UpworkFeedSearchBy.Score,
-                searchQuery: selectedScoresOptions.map((option) => option.value),
-            },
-            selectedReactionsOptions.length > 0 && {
-                searchBy: UpworkFeedSearchBy.Review,
-                searchQuery: selectedReactionsOptions.map((option) => option.value),
-            },
-        ].reduce<ISearchParameterDTO<UpworkFeedSearchBy>[]>((acc, param) => {
-            if (param) acc.push(param);
-            return acc;
-        }, []);
+        const searchParameters = createUpworkFeedsSearchParams(refetchSearchParams);
 
         getUpworkFeeds({
             searchParameters,
@@ -66,7 +36,7 @@ const useUpworkFeedRequests = (
         });
     };
 
-    return { data, refetchUpworkFeeds };
+    return { data, refetchUpworkFeeds, totalItemsCount, totalPagesCount };
 };
 
 export default useUpworkFeedRequests;
---
+++ src/pages/UpworkFeed/utils/createUpworkFeedsSearchParams.ts
@@ -0,0 +1,38 @@
+import { UpworkFeedSearchBy } from '../../../interfaces-submodule/enums/upwork-feed/upwork-feed-search-by.enum';
+import { ISearchParameterDTO } from '../../../interfaces-submodule/interfaces/dto/common/isearch-parameter.interface';
+import { IUpworkFeedRefetchSearchParams } from '../hooks/useUpworkFeedRequests';
+
+const createUpworkFeedsSearchParams = (refetchSearchParams: IUpworkFeedRefetchSearchParams) => {
+    const {
+        title,
+        published,
+        selectedKeywordsOptions,
+        selectedScoresOptions,
+        selectedReactionsOptions,
+    } = refetchSearchParams;
+
+    return [
+        title && { searchBy: UpworkFeedSearchBy.Title, searchQuery: title },
+        published && {
+            searchBy: UpworkFeedSearchBy.Published,
+            searchQuery: `${published.toISOString()} - ${new Date().toISOString()}`,
+        },
+        selectedKeywordsOptions.length > 0 && {
+            searchBy: UpworkFeedSearchBy.Keywords,
+            searchQuery: selectedKeywordsOptions.map((option) => option.value),
+        },
+        selectedScoresOptions.length > 0 && {
+            searchBy: UpworkFeedSearchBy.Score,
+            searchQuery: selectedScoresOptions.map((option) => option.value),
+        },
+        selectedReactionsOptions.length > 0 && {
+            searchBy: UpworkFeedSearchBy.Review,
+            searchQuery: selectedReactionsOptions.map((option) => option.value),
+        },
+    ].reduce<ISearchParameterDTO<UpworkFeedSearchBy>[]>((acc, param) => {
+        if (param) acc.push(param);
+        return acc;
+    }, []);
+};
+
+export default createUpworkFeedsSearchParams;
