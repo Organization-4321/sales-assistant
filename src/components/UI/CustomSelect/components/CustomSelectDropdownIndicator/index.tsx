c77f3b300e8d8471177aa7b71bd152e564f4de36
xennvoid <thundvgod@gmail.com>
2024-10-08 14:47:11 +0300

    make CustomSelectDropdownIndicator icon themed

--- src/components/UI/CustomSelect/components/CustomSelectDropdownIndicator/index.tsx
+++ src/components/UI/CustomSelect/components/CustomSelectDropdownIndicator/index.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
 import { components, DropdownIndicatorProps } from 'react-select';
 import ArrowIconLeft from '../../../../icons/ArrowIconLeft';
+import { useAppTheme } from '../../../../../theme';
+import { ITheme } from '../../../../../theme/types';
 
 const CustomSelectDropdownIndicator: FC<DropdownIndicatorProps> = (props) => {
+    const theme = useAppTheme();
+
     return (
         <components.DropdownIndicator {...props}>
-            <ArrowIconLeft sx={{ width: '16px', height: '16px', transform: 'rotate(-90deg)' }} />
+            <ArrowIconLeft
+                sx={{
+                    width: '16px',
+                    height: '16px',
+                    transform: 'rotate(-90deg)',
+                    color: theme.themeName === ITheme.DARK ? '#ebecf0' : '#333333',
+                }}
+            />
         </components.DropdownIndicator>
     );
 };
