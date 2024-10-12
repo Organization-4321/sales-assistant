e2edfdf5f11067b326a7c1a30a9f06d4e26edbdd
xennvoid <thundvgod@gmail.com>
2024-10-12 14:32:07 +0300

    replace Auth page redirect logic with useRedirect hook and wrap page with Paper

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,49 +1,45 @@
 import { FC } from 'react';
-import { Grid, Stack } from '@mui/material';
+import { Grid, Paper, Stack } from '@mui/material';
 import { useAppTheme } from '../../theme';
 import AuthForm from './components/AuthForm';
 import AuthFormTitles from './components/AuthFormTitles';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import { Navigate } from 'react-router-dom';
-import { APP_ROUTES } from '../../routes/app-routes.enum';
+import useRedirect from '../../hooks/useRedirect';
 
 interface AuthProps {}
 
 const Auth: FC<AuthProps> = ({}) => {
     const { themeName } = useAppTheme();
-    const { user } = useSelector((state: RootState) => state.currentUser);
 
-    if (user) {
-        return <Navigate to={APP_ROUTES.UPWORK_FEED} />;
-    }
+    useRedirect();
 
     return (
-        <Grid container>
-            <Grid
-                item
-                md="auto"
-                sx={{
-                    width: { md: '440px' },
-                    paddingX: { md: 7.5 },
-                    paddingY: { md: 12 },
-                }}>
-                <Stack direction="column">
-                    <AuthFormTitles />
-                    <AuthForm />
-                </Stack>
+        <Paper>
+            <Grid container>
+                <Grid
+                    item
+                    md="auto"
+                    sx={{
+                        width: { md: '440px' },
+                        paddingX: { md: 7.5 },
+                        paddingY: { md: 12 },
+                    }}>
+                    <Stack direction="column">
+                        <AuthFormTitles />
+                        <AuthForm />
+                    </Stack>
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    md
+                    sx={{
+                        backgroundImage: `url("/src/assets/images/auth-${themeName}-bg.jpg")`,
+                        backgroundRepeat: `no-repeat`,
+                        minHeight: '100vh',
+                    }}
+                />
             </Grid>
-            <Grid
-                item
-                xs={12}
-                md
-                sx={{
-                    backgroundImage: `url("/src/assets/images/auth-${themeName}-bg.jpg")`,
-                    backgroundRepeat: `no-repeat`,
-                    minHeight: '100vh',
-                }}
-            />
-        </Grid>
+        </Paper>
     );
 };
 
