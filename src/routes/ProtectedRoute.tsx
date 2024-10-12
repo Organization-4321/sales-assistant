4d9439a22797a20b88c36add35b10494275203fd
xennvoid <thundvgod@gmail.com>
2024-10-12 14:29:43 +0300

    add state with location to ProtectedRoute Navigate component

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { APP_ROUTES } from './app-routes.enum';
 
 const ProtectedRoute: FC = () => {
     const { user } = useSelector((state: RootState) => state.currentUser);
+    const location = useLocation();
 
-    return user ? <Outlet /> : <Navigate to={APP_ROUTES.LOGIN} replace />;
+    return user ? (
+        <Outlet />
+    ) : (
+        <Navigate to={APP_ROUTES.LOGIN} replace state={{ from: location.pathname }} />
+    );
 };
 
 export default ProtectedRoute;
