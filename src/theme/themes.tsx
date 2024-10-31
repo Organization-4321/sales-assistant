ea90864ec107ffc7e9a235cdc52b3b4ad886b9cd
xennvoid <thundvgod@gmail.com>
2024-10-31 12:05:09 +0200

    add borders property to the theme

--- src/theme/themes.tsx
+++ src/theme/themes.tsx
@@ -14,6 +14,7 @@ const createAppTheme = (mode: ITheme) => {
 
     const theme = createTheme({
         typography: baseTypography,
+        borders: { thin: `1px solid ${isDarkMode ? '#414752' : '#d5d7db'}` },
     });
 
     return createTheme(theme, {
